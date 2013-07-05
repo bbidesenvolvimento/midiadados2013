b104571cc8830b9ee403bb12d42b68b030354e27
BBI <BBI@BBIs-Mac-mini.local>
2013-07-05 05:22:25 -0300

    GA
    GA

--- ios/v_1_1/ios/midiadadospro2013/www/exclusive.js
+++ ios/v_1_1/ios/midiadadospro2013/www/exclusive.js
@@ -10,14 +10,16 @@
 
 function startGA(){
     console.log('GA STARTED')
- //  window.plugins.analytics.start("UA-41162029-1", function(){console.log("GA SUCCESS");}, function(){console.log("GA: failure");});
+    ga_storage._setAccount('UA-42254608-1'); //Replace with your own
+    ga_storage._trackPageview('/index.html');
+ //  window.plugins.analytics.start("UA-42254608-1", function(){console.log("GA SUCCESS");}, function(){console.log("GA: failure");});
 }
 
 
 
 
 function trackAndroidGAEvent(category, action, label, value){
-    console.log('trackAndroidGAEvent',1,category,2, action,3, label,4, value)
+    ga_storage._trackEvent('trackAndroidGAEvent',1,category,2, action,3, label,4, value)
  //   window.plugins.analytics.trackEvent(category, action, event, 1, onSuccessGAEvent,onFailGAEvent);
 }
 
