0d054dd7affee32888ff1c2c9d447fb214654dc7
filiperp <filiperp@gmail.com>
2013-07-16 12:47:48 -0300

    GA no export

--- _genericWWW/scripts_mdb/export.php
+++ _genericWWW/scripts_mdb/export.php
@@ -50,3 +50,18 @@ function get_trusted_ticket($wgserver, $user, $remote_addr) {
    	 </center>
 </iframe>
 </div>
+
+<script type="text/javascript">
+  var _gaq = _gaq || [];
+    _gaq.push(['_setAccount', 'UA-42475130-1']);
+    _gaq.push(['_trackPageview']);
+
+    (function() {
+        var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+        ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+        var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+    })();
+
+    _gaq.push(['_setDomainName', 'none']);
+    _gaq.push(['_setAllowLinker', true]);
+    </script>
