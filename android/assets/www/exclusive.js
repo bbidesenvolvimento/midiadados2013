2db510d70701f75eac9213d4788b86db9cc5ebd1
filiperp <filiperp@gmail.com>
2013-07-16 12:28:15 -0300

    GA disabled in mobile

--- android/assets/www/exclusive.js
+++ android/assets/www/exclusive.js
@@ -9,14 +9,14 @@
 
 
 function startGA(){
-    window.plugins.analytics.start("UA-41162029-1", function(){console.log("GA SUCCESS");}, function(){console.log("GA: failure");});
+   // window.plugins.analytics.start("UA-41162029-1", function(){console.log("GA SUCCESS");}, function(){console.log("GA: failure");});
 }
 
 
 
 
 function trackAndroidGAEvent(category, action, label, value){
-    window.plugins.analytics.trackEvent(category, action, event, 1, onSuccessGAEvent,onFailGAEvent);
+  //  window.plugins.analytics.trackEvent(category, action, event, 1, onSuccessGAEvent,onFailGAEvent);
 }
 
 function onSuccessGAEvent(){
--- ios/v_1_1/ios/midiadadospro2013/www/exclusive.js
+++ ios/v_1_1/ios/midiadadospro2013/www/exclusive.js
@@ -9,9 +9,9 @@
 
 
 function startGA(){
-    console.log('GA STARTED')
-    ga_storage._setAccount('UA-42254608-1'); //Replace with your own
-    ga_storage._trackPageview('/index.html');
+  //  console.log('GA STARTED')
+  //  ga_storage._setAccount('UA-42254608-1'); //Replace with your own
+   // ga_storage._trackPageview('/index.html');
  //  window.plugins.analytics.start("UA-42254608-1", function(){console.log("GA SUCCESS");}, function(){console.log("GA: failure");});
 }
 
@@ -19,7 +19,7 @@ function startGA(){
 
 
 function trackAndroidGAEvent(category, action, label, value){
-    ga_storage._trackEvent('trackAndroidGAEvent',1,category,2, action,3, label,4, value)
+  //  ga_storage._trackEvent('trackAndroidGAEvent',1,category,2, action,3, label,4, value)
  //   window.plugins.analytics.trackEvent(category, action, event, 1, onSuccessGAEvent,onFailGAEvent);
 }
 
